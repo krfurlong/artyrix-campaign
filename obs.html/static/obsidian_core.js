8572200c81c5bc3cc01ea4f105666b2cceb3e87b
krfurlong <krfurlong@users.noreply.github.com>
2025-05-15 20:11:33 +0000

    deploy: 12f2d99a5c699028007383636009730db00ad5c9

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '135982982196093538237999857362175344034'                       // used to check whether the localStorage data is stale
+var gzip_hash = '224743924721569295869433688900191169184'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
